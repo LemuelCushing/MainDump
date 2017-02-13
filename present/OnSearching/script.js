a91cf5bbc4ba9aa0de644e78f82021802e35b95d
LemuelCushing <SeventhHorror@gmail.com>
2017-02-13 13:30:11 +0200

    Tweak attempts

---
+++ present/OnSearching/script.js
@@ -0,0 +1,20 @@
+  $("button").on('click',function(){
+    console.log("img Ready!");
+  });
+  // $(".imgClick").click(function(){
+  //   var categories = "";
+  //   switch(this.id){
+  //     case "1": categories = "news,history";
+  //     break;
+  //     case "2": categories = "newspaper,archive";
+  //     break;
+  //     case "3": categories = "jewish,newspaper,history";
+  //     break;
+  //     case "4": categories = "archive,history,newspaper";
+  //     break;
+  //   }
+  //   console.log(categories);
+  //   console.log("bob");
+  //   $(this).fadeOut("slow",function(){
+  //     $(this).html('<img src="https://loremflickr.com/g/700/100/'+categories+'/all?random=">').fadeIn(3000);});
+  // });
